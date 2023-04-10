18babd1fa8eddd4e6df6880230bf90eb45f0a013
Parisa Adib <parisaadib78@gmail.com>
2023-04-10 18:24:19 +0430

    import cartContextProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import { useState } from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom'; 
+
 //components
 import ProductDetials from './components/ProductDetials';
 import Store from './components/Store';
+
 //context
 import ProductContextProvider from './context/ProductContextProvider';
+import CartContextProvider from './context/CartContextProvider';
 
 function App() {
   return (
     <ProductContextProvider>
-      <Switch>
-        <Route path="/product/:id" component={ProductDetials} />
-        <Route path="/product" component={Store} />
-        <Redirect to="/product" />
-      </Switch>
+      <CartContextProvider>
+          <Switch>
+            <Route path="/product/:id" component={ProductDetials} />
+            <Route path="/product" component={Store} />
+            <Redirect to="/product" />
+          </Switch>
+      </CartContextProvider>
     </ProductContextProvider>
   )
 }
