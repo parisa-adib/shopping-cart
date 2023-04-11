9f9c6fe8cfdea750e15d91cb35a4ae324b59c759
Parisa Adib <parisaadib78@gmail.com>
2023-04-12 00:57:20 +0430

    useContext , icon

--- src/components/shared/Product.jsx
+++ src/components/shared/Product.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+
 //functions
-import { shorten } from '../../helper/functions';
+import { shorten, isInCart, quantityCount } from '../../helper/functions';
+
+//context
+import { CartContext } from '../../context/CartContextProvider';
+
+//icons
+import trashIcon from '../../asset/icons/trash.svg';
 
 const Product = ({productData}) => {
+
+    const {state, dispatch} = useContext(CartContext);
+
     return (
         <div className='mt-5 border-opacity-20 border border-gray-500 rounded-md shadow-md shadow-gray-300'>
             <img src={productData.image} alt="product" className='w-52 h-48' />
@@ -13,9 +23,17 @@ const Product = ({productData}) => {
                 <div>
                     <Link to={`/product/${productData.id}`}>details</Link>
                     <div>
-                        <button className='bg-green-600 rounded-md text-white p-1'>
-                            add to cart
-                        </button>
+                        {
+                            isInCart(state, productData.id) ? 
+                            <button onClick={() => dispatch({type: "INCREASE", payload: productData})}>+</button> :
+                            <button onClick={() => dispatch({type: "ADD_ITEM", payload: productData})}>add to cart</button>
+                        }
+                        {quantityCount(state, productData.id) > 1 && 
+                            <button onClick={() => dispatch({type: "DECREASE", payload: productData})}>-</button> }
+                        {quantityCount(state, productData.id) === 1 &&
+                            <button onClick={() => dispatch({type: "REMOVE_ITEM", payload: productData})}>
+                             <img src={trashIcon} alt='trash' style={{width:"20px"}}/>
+                            </button>}
                     </div>
                 </div>
             </div>
