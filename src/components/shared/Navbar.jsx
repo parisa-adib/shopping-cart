3764fa7a4c2067bf3369fa482131986ead451899
Parisa Adib <parisaadib78@gmail.com>
2023-04-14 01:16:17 +0430

    icon & quantity added

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -3,10 +3,21 @@ import React, { useContext } from 'react';
 //context
 import { CartContext } from '../../context/CartContextProvider';
 
+//icon
+import basketIcon from '../../asset/icons/shopping-basket.svg';
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     const {state} = useContext(CartContext)
     return (
         <div>
+            <div>
+                <Link to='/product'>Products</Link>
+                <div>
+                    <Link to='/Cart' ><img src={basketIcon} alt='shop' /></Link>
+                    <span>{state.itemsCounter}</span>
+                </div>
+            </div>
             
         </div>
     );
