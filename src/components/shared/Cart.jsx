f20cf8943e0f6975f1b491d929a62e671e6b6534
Parisa Adib <parisaadib78@gmail.com>
2023-04-15 23:58:12 +0430

    import style

--- src/components/shared/Cart.jsx
+++ src/components/shared/Cart.jsx
@@ -9,23 +9,26 @@ import { shorten } from '../../helper/functions';
 //icon
 import trashIcon from '../../asset/icons/trash.svg';
 
+//style
+import styles from './Cart.module.css';
+
 const Cart = (props) => {
     const { dispatch } = useContext(CartContext);
     const { image, title, quantity, price } = props.data;
     return (
-        <div>
-            <img src={image} alt='image' />
-            <div>
+        <div className={styles.container}>
+            <img src={image} alt='image' className={styles.productImage}/>
+            <div className={styles.data}>
                 <h3>{shorten(title)}</h3>
                 <p>{price} $</p>
                 <div>
-                    <span>{quantity}</span>
+                    <span className={styles.quantity}>{quantity}</span>
                 </div>
-                <div>
+                <div className={styles.buttonContainer}>
                     {
                         quantity > 1 ? 
-                        <button onClick={() => dispatch({type: "DECREASE" , payload: props.data})}>-</button> :
-                        <button onClick={() => dispatch({type: "REMOVE_ITEM" , payload: props.data})}><img src={trashIcon} alt='trash' style={{width:"20px"}} /></button>
+                        <button onClick={() => dispatch({type: "DECREASE" , payload: props.data})} >-</button> :
+                        <button onClick={() => dispatch({type: "REMOVE_ITEM" , payload: props.data})}><img src={trashIcon} alt='trash' className='w-5' /></button>
                     }
                     <button onClick={() => dispatch({type: "INCREASE", payload: props.data})}>+</button>
                 </div>
