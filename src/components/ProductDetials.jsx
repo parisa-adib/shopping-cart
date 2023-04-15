ff7f5d9db1b431f219fa45a4d6c4620fbbee6d1d
Parisa Adib <parisaadib78@gmail.com>
2023-04-15 23:57:39 +0430

    import style

--- src/components/ProductDetials.jsx
+++ src/components/ProductDetials.jsx
@@ -3,20 +3,23 @@ import React, { useContext } from 'react';
 import { ProductContext } from '../context/ProductContextProvider';
 import { Link } from 'react-router-dom';
 
+//style
+import styles from "./ProductDetails.module.css";
+
 const ProductDetials = (props) => {
     const id = props.match.params.id;
     const data = useContext(ProductContext);
     const product = data[id - 1];
     const { image, title, description, price, category } = product;
     return (
-        <div>
-           <img src={image} alt='productImg' />
-           <div>
+        <div className={styles.container}>
+           <img src={image} alt='productImg' className={styles.image} />
+           <div className={styles.textContainer}>
             <h3> {title} </h3>
-            <p> {description} </p>
-            <p> {category} </p>
-            <div>
-                 <span> {price} </span>
+            <p className={styles.description}> {description} </p>
+            <p className={styles.category}> {category} </p>
+            <div className={styles.buttonContainer}>
+                 <span className={styles.price}> {price} </span>
                  <Link to="/product">back to shop</Link>
             </div>
            </div>
