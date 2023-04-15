83a2c198cb236def188dbfee9716bdddf9869250
Parisa Adib <parisaadib78@gmail.com>
2023-04-16 00:00:01 +0430

    navbar styles with tailwind

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -10,12 +10,12 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     const {state} = useContext(CartContext)
     return (
-        <div>
-            <div>
-                <Link to='/product'>Products</Link>
-                <div>
+        <div className='fixed w-full shadow-lg top-0 mt-0 mx-0 mb-44 bg-white'>
+            <div className='flex justify-between items-center h-14 px-44 py-8'>
+                <Link to='/product' className='text-[#1a73e8] font-bold hover:text-[#0b499b]'>Products</Link>
+                <div className='relative'>
                     <Link to='/Cart' ><img src={basketIcon} alt='shop' /></Link>
-                    <span>{state.itemsCounter}</span>
+                    <span className='absolute top-0 right-0 bg-[#0b499b] rounded-full text-white w-4 h-4 text-center font-bold text-xs'>{state.itemsCounter}</span>
                 </div>
             </div>
             
