a2243b38431aac3006421f6ce49906320426d3d2
Parisa Adib <parisaadib78@gmail.com>
2023-04-15 23:59:11 +0430

    import style

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -3,11 +3,13 @@ import React, { useContext } from 'react';
 import Product from './shared/Product';
 //context
 import { ProductContext } from '../context/ProductContextProvider';
+//style
+import styles from "./Store.module.css";
 
 const Store = () => {
     const products = useContext(ProductContext)
     return (
-        <div className='flex flex-wrap justify-between mt-4'>
+        <div className={styles.container}>
             {
             products.map(product => <Product 
                                         key={product.id} 
